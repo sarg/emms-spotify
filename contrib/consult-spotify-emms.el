91fe535d3420fa57c24ef164bf4b47085ed3cd6a
Sergey Trofimov <sarg@sarg.org.ru>
2026-01-09 20:35:52 +0100

    contrib: hide variable containing api token

--- contrib/consult-spotify-emms.el
+++ contrib/consult-spotify-emms.el
@@ -24,7 +24,7 @@
 
 ;;;; Custom
 (defgroup consult-spotify-emms nil
-  "Access to Spotify API and clients"
+  "Access to Spotify API and clients."
   :group 'multimedia)
 
 (defcustom consult-spotify-emms-client-id nil
@@ -44,41 +44,44 @@
 
 ;;;; Spotify API
 (defmacro consult-spotify-emms--secret-value (name)
-  `(cl-typecase ,name
-     (function (funcall ,name))
-     (string consult-spotify-emms-client-id)
-     (t (user-error ,(concat (symbol-name name) " is not set")))))
+  "Extract secret NAME according to its type."
+  `(cond
+    ((functionp ,name) (funcall ,name))
+    ((characterp ,name) consult-spotify-emms-client-id)
+    (t (user-error ,(concat (symbol-name name) " is not set")))))
 
-(defvar consult-spotify-emms--cached-token nil)
-(defun consult-spotify-emms--refresh-token ()
-  (message "Authorizing")
-  (request "https://accounts.spotify.com/api/token"
-    :headers `(("Content-Type" . "application/x-www-form-urlencoded")
-               ("Authorization" .
-                ,(concat "Basic "
-                         (base64-encode-string
-                          (concat
-                           (consult-spotify-emms--secret-value consult-spotify-emms-client-id) ":"
-                           (consult-spotify-emms--secret-value consult-spotify-emms-client-secret))
-                          'no-line-break))))
-    :type "POST"
-    :params '((grant_type . client_credentials))
-    :parser #'json-read 
-    :success (cl-function
-              (lambda (&key data &allow-other-keys)
-                (setq consult-spotify-emms--cached-token
-                      (alist-get 'access_token data))))
-    :error (cl-function
-            (lambda (&key error-thrown &allow-other-keys)
-              (user-error "Failed to get access token: %s" error-thrown)))
-    :sync t))
+(declare-function consult-spotify-emms--auth-token "consult-spotify-emms")
+(let ((token))
+  (defun consult-spotify-emms--auth-token (&optional force)
+    (when (or (null token) force)
+      (request "https://accounts.spotify.com/api/token"
+        :headers `(("Content-Type" . "application/x-www-form-urlencoded")
+                   ("Authorization" .
+                    ,(concat "Basic "
+                             (base64-encode-string
+                              (concat
+                               (consult-spotify-emms--secret-value consult-spotify-emms-client-id) ":"
+                               (consult-spotify-emms--secret-value consult-spotify-emms-client-secret))
+                              'no-line-break))))
+        :type "POST"
+        :params '((grant_type . client_credentials))
+        :parser #'json-read
+        :success (cl-function
+                  (lambda (&key data &allow-other-keys)
+                    (setq token (alist-get 'access_token data))))
+        :error (cl-function
+                (lambda (&key error-thrown &allow-other-keys)
+                  (user-error "Failed to get access token: %s" error-thrown)))
+        :sync t))
+    token))
 
 (defun consult-spotify-emms--api-request (path &rest args)
+  "Make a request to spotify api endpoint at PATH. Add ARGS to request call."
   (declare (indent 1))
   (let ((retries 0) result)
     (while (and (< retries 2) (not result))
       (apply #'request (concat "https://api.spotify.com/v1" path)
-             :headers `(("Authorization" . ,(concat "Bearer " consult-spotify-emms--cached-token)))
+             :headers `(("Authorization" . ,(concat "Bearer " (consult-spotify-emms--auth-token))))
              :sync t
              :parser (lambda () (let ((json-array-type 'list)) (json-read)))
              :success (cl-function (lambda (&key data &allow-other-keys)
@@ -91,11 +94,12 @@
 
       (when (eq result 'unauthorized)
         (setq retries (+ retries 1) result nil)
-        (consult-spotify-emms--refresh-token)))
+        (consult-spotify-emms--auth-token 'force)))
     result))
 
 ;;;; Consult functions
 (defun consult-spotify-emms--format (data)
+  "Format json DATA for display."
   (let-alist data
     (concat
      (truncate-string-to-width .name consult-spotify-emms-max-name-length nil nil 'ellipsis)
@@ -111,7 +115,7 @@
        (?p (concat " by " (alist-get 'display_name .owner)))))))
 
 (defun consult-spotify-emms--search (input &optional type)
-  "Search albums, playlists and tracks given user INPUT."
+  "Search itsm of TYPE (albums, playlists, tracks) given user INPUT."
   (declare (indent 1))
   (thread-last
     (consult-spotify-emms--api-request "/search"
@@ -157,7 +161,7 @@
 
 ;;;; EMMS functions 
 (defun consult-spotify-emms--make-track (item)
-  "Make emms-track out of spotify's json."
+  "Make emms-track out of spotify's json ITEM."
   (let-alist item
     (let ((track (emms-track 'url .uri)))
       (emms-track-set track 'info-title .name)
